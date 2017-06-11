c241d8e10d23edf6ddfde896e171fe13bec43de7
Roopesh Reddy C <rreddyc@phx-m-lpt90103.home>
2017-06-11 13:43:10 -0700

    Initial commit

---
+++ app/src/main/java/com/xxx/rreddyc/sample/Utilities/Utility.java
@@ -0,0 +1,14 @@
+package com.xxx.rreddyc.sample.Utilities;
+
+/**
+ * Created by rreddyc on 6/10/17.
+ */
+
+public class Utility {
+    private String name;
+
+
+    public Utility(String name) {
+        this.name = name;
+    }
+}
